24e333948fc5135f0e31036ef886f4583615bbde
Estudiante <Estudiante@DESKTOP-BDCDHV4>
2023-04-14 09:22:20 +0200

    Agregado nombre

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -3,7 +3,12 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hola");
+		
+		String nombre = "Alonso";
+		
+		
 	}
 
 }
